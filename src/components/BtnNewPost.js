f0d960c6d704d01b7b560343860a2631089b6b61
adrianlouis <adrian.louis.003@live.com>
2024-01-30 15:28:21 -0300

    filter button

--- src/components/BtnNewPost.js
+++ src/components/BtnNewPost.js
@@ -5,14 +5,26 @@ import Filter from './Filter';
 const BtnNewPost = ({ onclick }) => {
   function hover(elem) {
     const filter = elem.firstChild.nextElementSibling;
-    filter.style.right = '3rem';
+    filter.classList.add('animeIcon');
+    elem.style.width = '5rem';
+    // elem.style.height = '5rem';
   }
+
+  function blur(elem) {
+    const blur = elem.firstChild.nextElementSibling;
+    elem.style.width = '2rem';
+    elem.style.height = '2rem';
+    blur.classList.remove('animeIcon');
+  }
+
   return (
     <div
-      className={styles.container}
+      className={`${styles.container} animateLeft`}
       onMouseEnter={({ currentTarget }) => hover(currentTarget)}
+      onMouseLeave={({ currentTarget }) => blur(currentTarget)}
+      // onMouseLeave={({ currentTarget }) => blur(currentTarget)}
     >
-      <div className={`${styles.newPost} animateLeft`} onClick={onclick}>
+      <div className={styles.newPost} onClick={onclick}>
         <i className="fa-solid fa-circle-plus"></i>
       </div>
       <Filter />
